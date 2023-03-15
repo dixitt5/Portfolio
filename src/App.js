8a254ef9d7fc81e14c7799b99965c22bc8fe5d6e
dixitt5 <tilajidixit@gmail.com>
2023-03-16 00:45:17 +0530

    skilss and about comp added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from "react";
 // import components
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-
+import Brands from "./components/Brands";
+import About from "./components/About";
+import Skils from "./components/Skils";
 const App = () => {
   return (
     <div>
       <Header />
-      <Hero/>
+      <Hero />
+      <Brands />
+      <About />
+      <Skils/>
       <div style={{ height: "2000px" }}></div>
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "../assets/img/about.webp";
+const About = () => {
+  return (
+    <section className="section bg-secondary">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-24 ">
+          <img
+            className="object-cover h-full w-[566px] md:mx-auto max-w-fit rounded-[12px]"
+            src={Image}
+            alt=""
+          />
+          <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
+            <div className="flex flex-col">
+              <h2 className="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before::opacity-40 before:-top-[2rem] before:hidden before:lg:block">
+                Dixit Tilaji
+              </h2>
+              <p className="mb-4 text-accent">Web Developer</p>
+              <hr className="mb-8 opacity:5" />
+              <p className="mb-8">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Exercitationem magni ex, cum nulla aperiam autem quia aliquid
+                blanditiis eaque recusandae! <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+                debitis, voluptas maiores corporis exercitationem quae
+                laboriosam adipisci modi impedit, similique atque. Quibusdam
+                autem suscipit consequatur iusto architecto atque molestiae
+                nulla!
+              </p>
+            </div>
+            <button className="btn btn-md bg-accent hover:bg-accent-hover transition-all">
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
